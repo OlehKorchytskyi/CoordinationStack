2fd30c38d482cf00a4f19bf289eee545258b9580
Oleh Korchytskyi <oleh.korchytskyi@gmail.com>
2025-06-25 05:25:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CoordinationStack",
-            type: .dynamic,
-            targets: ["CoordinationStack"]),
+            targets: ["CoordinationStack"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
